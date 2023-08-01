ea63fa43c4961ca05f96e896b9db3c486d91eba4
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-08-01 22:43:02 +0300

    Home page continues

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Profile from '../../assets/lionel.jpeg';
 import { Link } from 'react-router-dom';
 import { FaArrowRight } from "react-icons/fa";
-import ".home.css";
+import "./home.css";
 
 const Home = () => {
     return (
