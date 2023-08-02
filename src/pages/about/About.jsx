ef091f3673b42a825e4d81393f3a363f24e749e3
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-08-02 21:44:35 +0300

    Finalizing the about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Info from '../../components/Info';
 import Stats from '../../components/Stats';
 import Skills from '../../components/Skills';
+import ResumeItem from '../../components/ResumeItem';
+import { resume } from '../../data';
 import { FaDownload } from "react-icons/fa";
 import CV from "../../assets/Resume.pdf";
 import "./about.css";
@@ -47,7 +49,28 @@ const About = () => {
 
             <div className="separator"></div>
 
-            
+            <section className="resume">
+                <h3 className="section__subtitle subtitle__center">
+                    Experience & Education
+                </h3>
+
+                <div className="resume__container grid">
+                    <div className="resume__data">
+                        {resume.map((val) => {
+                            if (val.category === 'experience'){
+                                return <ResumeItem key={val.id} {...val} />;
+                            }
+                        })}
+                    </div>
+                    <div className="resume__data">
+                        {resume.map((val) => {
+                            if (val.category === 'education'){
+                                return <ResumeItem key={val.id} {...val} />;
+                            }
+                        })}
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
